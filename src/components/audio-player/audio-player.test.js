7ecd044c2c27f98be6f1d9422cb77754d61ded7f
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-06-24 15:52:00 +0500

    Добавляет ref

--- src/components/audio-player/audio-player.test.js
+++ src/components/audio-player/audio-player.test.js
@@ -14,7 +14,11 @@ it(`AudioPlayer is rendered correctly`, () => {
   const tree = renderer.create(<AudioPlayer
     isPlaying={false}
     src={song.src}
-  />).toJSON();
+  />, {
+    createNodeMock: () => {
+      return {};
+    }
+  }).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
